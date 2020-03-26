8f4b0df79315a9915797f69cd707817266c3db31
Шафигуллин Илдар <ishafigullin@masterdm.ru>
2020-03-26 17:35:20 +0300

    Source-Level Annotation Processing fix.

--- src/v2ch08/rect/Rectangle.java
+++ src/v2ch08/rect/Rectangle.java
@@ -4,15 +4,29 @@ import v2ch08.sourceAnnotations.ToString;
 
 @ToString
 public class Rectangle {
+
     private Point topLeft;
     private int width;
     private int height;
+
     public Rectangle(Point topLeft, int width, int height) {
         this.topLeft = topLeft;
         this.width = width;
         this.height = height;
     }
-    @ToString(includeName=false) public Point getTopLeft() { return topLeft; }
-    @ToString public int getWidth() { return width; }
-    @ToString public int getHeight() { return height; }
+
+    @ToString(includeName = false)
+    public Point getTopLeft() {
+        return topLeft;
+    }
+
+    @ToString
+    public int getWidth() {
+        return width;
+    }
+
+    @ToString
+    public int getHeight() {
+        return height;
+    }
 }
--- src/v2ch08/rect/SourceLevelAnnotationDemo.java
+++ src/v2ch08/rect/SourceLevelAnnotationDemo.java
@@ -1,12 +1,17 @@
 package v2ch08.rect;
 
-//import v2ch08.sourceAnnotations.ToStrings;
+import v2ch08.sourceAnnotations.ToStrings;
 
-public class SourceLevelAnnotationDemo
-{
-   public static void main(String[] args)
-   {
-      Rectangle rect = new Rectangle(new Point(10, 10), 20, 30);
-      //System.out.println(ToStrings.toString(rect));
-   }
+/**
+ * Using orders:
+ * javac v2ch08/sourceAnnotations/ToStringAnnotationProcessor.java
+ * javac -processor v2ch08.sourceAnnotations.ToStringAnnotationProcessor v2ch08/rect/*.java
+ * java v2ch08.rect.SourceLevelAnnotationDemo
+ */
+public class SourceLevelAnnotationDemo {
+
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle(new Point(10, 10), 20, 30);
+        System.out.println(ToStrings.toString(rect));
+    }
 }
