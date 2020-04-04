9086b0763bdd4dd30db9343050da6a2e59e49b46
Шафигуллин Илдар <ishafigullin@masterdm.ru>
2020-04-04 17:30:27 +0300

    Add functionality: choose rundom number letters in list.

--- src/v2ch10/longList/LongListFrame.java
+++ src/v2ch10/longList/LongListFrame.java
@@ -8,39 +8,48 @@ import javax.swing.*;
  * This frame contains a long word list and a label that shows a sentence made up from the chosen
  * word.
  */
-public class LongListFrame extends JFrame
-{
-   private JList<String> wordList;
-   private JLabel label;
-   private String prefix = "The quick brown ";
-   private String suffix = " jumps over the lazy dog.";
+public class LongListFrame extends JFrame {
 
-   public LongListFrame()
-   {
-      wordList = new JList<>(new WordListModel(3));
-      wordList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-      wordList.setPrototypeCellValue("www");
-      JScrollPane scrollPane = new JScrollPane(wordList);
+    private static final int LETTER_COUNT = 5;
+    private static final String SINGLE_PROTOTYPE_CELL_VALUE = "w";
 
-      JPanel p = new JPanel();
-      p.add(scrollPane);
-      wordList.addListSelectionListener(event -> setSubject(wordList.getSelectedValue()));
+    private JList<String> wordList;
+    private JLabel label;
+    private String prefix = "The quick brown ";
+    private String suffix = " jumps over the lazy dog.";
 
-      Container contentPane = getContentPane();
-      contentPane.add(p, BorderLayout.NORTH);
-      label = new JLabel(prefix + suffix);
-      contentPane.add(label, BorderLayout.CENTER);
-      setSubject("fox");
-      pack();
-   }
+    public LongListFrame() {
+        wordList = new JList<>(new WordListModel(LETTER_COUNT));
+        wordList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        wordList.setPrototypeCellValue(computePrototypeCellValue(LETTER_COUNT));
+        JScrollPane scrollPane = new JScrollPane(wordList);
 
-   /**
-    * Sets the subject in the label.
-    * @param word the new subject that jumps over the lazy dog
-    */
-   public void setSubject(String word)
-   {
-      String text = prefix + word + suffix;
-      label.setText(text);
-   }
+        JPanel p = new JPanel();
+        p.add(scrollPane);
+        wordList.addListSelectionListener(event -> setSubject(wordList.getSelectedValue()));
+
+        Container contentPane = getContentPane();
+        contentPane.add(p, BorderLayout.NORTH);
+        label = new JLabel(prefix + suffix);
+        contentPane.add(label, BorderLayout.CENTER);
+        setSubject("fox");
+        pack();
+    }
+
+    private String computePrototypeCellValue(int letterCount) {
+        StringBuilder resultStr = new StringBuilder();
+        for (int i = 0; i < letterCount; i++) {
+            resultStr.append(SINGLE_PROTOTYPE_CELL_VALUE);
+        }
+        return resultStr.toString();
+    }
+
+    /**
+     * Sets the subject in the label.
+     * @param word the new subject that jumps over the lazy dog
+     */
+    public void setSubject(String word) {
+        String text = prefix + word + suffix;
+        label.setText(text);
+    }
 }
