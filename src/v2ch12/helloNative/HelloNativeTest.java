e2bad28ece75a7a60e21da97c6b6171fff231741
Шафигуллин Илдар <ishafigullin@masterdm.ru>
2020-04-26 17:01:45 +0300

    HelloNative Test comment

--- src/v2ch12/helloNative/HelloNativeTest.java
+++ src/v2ch12/helloNative/HelloNativeTest.java
@@ -1,18 +1,23 @@
 package v2ch12.helloNative;
 
 /**
- *  @version 1.11 2007-10-26
- *  @author Cay Horstmann
+ * In summary, follow these steps to link a native method to a Java program:
+ * 1. Declare a native method in a Java class.
+ * 2. Run javah to get a header file with a C declaration for the method.
+ * 3. Implement the native method in C.
+ * 4. Place the code in a shared library.
+ * 5. Load that library in your Java program.
+ *
+ * @author Cay Horstmann
+ * @version 1.11 2007-10-26
  */
-class HelloNativeTest
-{  
-   public static void main(String[] args)
-   {  
-      HelloNative.greeting();
-   }
+class HelloNativeTest {
 
-   static
-   {  
-      System.loadLibrary("v2ch12.helloNative.HelloNative");
-   }
+    public static void main(String[] args) {
+        HelloNative.greeting();
+    }
+
+    static {
+        System.loadLibrary("v2ch12.helloNative.HelloNative");
+    }
 }
